351ba4355759025f510b0edcddad78d05c2d872e
s1830134 <s1830134@s.tsukuba.ac.jp>
2021-03-13 17:50:17 +0900

    関数名の変更と追加

---
+++ src/packaging_tutorial/my_func.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+def my_add(x1: float, x2: float) -> float:
+    """ 足し算を行う (x1 + x2)
+
+    Argumentns:
+        :x1: float
+            数値1
+
+        :x2: float
+            数値2
+
+    Returns:
+        :y: float
+            x1+x2
+    """
+
+    y = x1 + x2
+    return y
+
+
+def my_sub(x1: float, x2: float) -> float:
+    """ 引き算を行う (x1 + x2)
+
+    Argumentns:
+        :x1: float
+            数値1
+
+        :x2: float
+            数値2
+
+    Returns:
+        :y: float
+            x1-x2
+    """
+
+    y = x1 - x2
+    return y
