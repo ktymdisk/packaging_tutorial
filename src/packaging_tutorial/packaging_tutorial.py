26303a82f18b53891f609044bc91955b9c167534
s1830134 <s1830134@s.tsukuba.ac.jp>
2021-03-13 17:38:12 +0900

    initial commit

---
+++ src/packaging_tutorial/packaging_tutorial.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def test_my_add(x1: float, x2: float) -> float:
+    """ 足し算を行う
+
+    Argumentns:
+        :x1: float
+            数値1
+
+        :x2: float
+            数値2
+
+    Returns:
+        :y: float
+            x1+x2
+    """
+
+    y = x1 + x2
+    return y
