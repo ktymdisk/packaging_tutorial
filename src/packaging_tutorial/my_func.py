ddb7e54bba7ebf5ad904c5aba8ca88ee45f6f719
s1830134 <s1830134@s.tsukuba.ac.jp>
2021-03-14 14:21:04 +0900

    add my_mul

--- src/packaging_tutorial/my_func.py
+++ src/packaging_tutorial/my_func.py
@@ -12,7 +12,7 @@ def my_add(x1: float, x2: float) -> float:
 
     Returns:
         :y: float
-            x1+x2
+            x1 + x2
     """
 
     y = x1 + x2
@@ -31,8 +31,27 @@ def my_sub(x1: float, x2: float) -> float:
 
     Returns:
         :y: float
-            x1-x2
+            x1 - x2
     """
 
     y = x1 - x2
+    return y
+
+
+def my_mul(x1: float, x2: float) -> float:
+    """ 掛け算を行う (x1 * x2)
+
+    Argumentns:
+        :x1: float
+            数値1
+
+        :x2: float
+            数値2
+
+    Returns:
+        :y: float
+            x1 * x2
+    """
+
+    y = x1 * x2
     return y
--- tests/test_my_func.py
+++ tests/test_my_func.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from packaging_tutorial import my_func
+import pytest
 
 
 def test__my_add():
@@ -8,4 +9,8 @@ def test__my_add():
 
 
 def test__my_sub():
-    assert my_func.my_sub(1.2, 1.3) == -0.1
+    assert my_func.my_sub(1.2, 1.3) == pytest.approx(-0.1)
+
+
+def test__my_mul():
+    assert my_func.my_mul(2.0, 1.3) == 2.6
